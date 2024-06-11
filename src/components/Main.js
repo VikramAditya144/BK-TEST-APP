050b52da03de66028bd6342fe075c4704ce4324a
Vikramaditya <vikramaditya.@Vikramadityas-MacBook-Air.local>
2024-06-11 21:31:29 +0530

    make home and quiz pages

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import React , {useRef}from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Main.css'
+export default function Main(){
+    const inputRef = useRef(null)
+    return (
+        <div className='container'>
+            <h1 className='title text-light'>Om Shanti</h1>
+
+            <ol>
+                <li>
+                    Solve All the Questions
+                </li>
+
+                <li>
+                    Complete in Allocated time
+                </li>
+                <li>
+                    Be Honest , Give your Best
+                </li>
+            </ol>
+
+            <form id= "form">
+                <input  ref = {inputRef} className='userid' type='text' placeholder='Username'></input>
+                <input  ref = {inputRef} className='userid' type='tel' placeholder='Phone No'></input>
+                {/* <input type='number' placeholder='Number'></input> */}
+            </form>
+
+            <div className='start'>
+                <Link className='btn' to={'quiz'}>Start Test</Link>
+            </div>
+        </div>
+    )
+}
---
+++ src/database/data.js
@@ -0,0 +1,41 @@
+export default [
+    {
+        id:1,
+        question :"Javascript is a ________ Language",
+        options: [
+            'good',
+            'Procedural',
+            'OOP'
+        ]
+    },
+
+    {
+        id:2,
+        question :"Javascript is a ________ Language",
+        options: [
+            'good',
+            'Procedural',
+            'OOP'
+        ]
+    },
+
+    {
+        id:3,
+        question :"Javascript is a ________ Language",
+        options: [
+            'good',
+            'Procedural',
+            'OOP'
+        ]
+    },
+
+    {
+        id:4,
+        question :"Javascript is a ________ Language",
+        options: [
+            'good',
+            'Procedural',
+            'OOP'
+        ]
+    },
+]
